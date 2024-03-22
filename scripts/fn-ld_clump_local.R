878e5a6f0e3ee83610bb01da6afde15bfedbc35c
Haotian <er20212@bristol.ac.uk>
2024-03-22 16:55:24 +0000

    add r2 threshold setting to clump function

--- scripts/fn-ld_clump_local.R
+++ scripts/fn-ld_clump_local.R
@@ -1,6 +1,6 @@
 # doing LD clump in local
 
-ld_clump_local =  function(out_dat, threshold = 5e-8) {
+ld_clump_local =  function(out_dat, threshold = 5e-8, r2 = 0.001) {
   
   if("pval.outcome" %in% colnames(out_dat)){
     tmp = subset(out_dat, pval.outcome < threshold) %>% TwoSampleMR::convert_outcome_to_exposure()
@@ -13,7 +13,7 @@ ld_clump_local =  function(out_dat, threshold = 5e-8) {
     dplyr::tibble(rsid = tmp$SNP,
                   pval = tmp$pval.exposure),
     clump_kb = 10000,
-    clump_r2 = 0.001,
+    clump_r2 = r2,
     clump_p = 0.99,
     plink_bin = genetics.binaRies::get_plink_binary(),
     bfile = paste0(rdsf_personal, "data/1kg_eur/EUR")
