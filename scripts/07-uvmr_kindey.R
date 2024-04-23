e813014b2e5fb7c31b100be837b5adf7d68f06a4
Haotian <er20212@bristol.ac.uk>
2024-04-23 16:27:33 +0100

    add ns from finngen r10 as outcome and avoid sample overlap

--- scripts/07-uvmr_kindey.R
+++ scripts/07-uvmr_kindey.R
@@ -1,8 +1,8 @@
-# doing MR for all exp on kindey function --------------------------------------
+# doing MR for all exp on kidney function --------------------------------------
 
 f2r_all_instruments = fread(paste0(rdsf_personal,"data/par1/f2r_all_instruments.csv"))
 
-kidney_outcomes = c("ns_meta","egfr_sd","ckd",
+kidney_outcomes = c("ns_meta","ns","egfr_sd","ckd",
                     "bun_sd","uacr","ma",
                     "ukb-d-30500_irnt","ukb-d-30600_irnt")
 
@@ -16,28 +16,26 @@ kidney_mr = rbind(uvmr(exp_name = "F2R kidney meta str", out_name = kidney_outco
 
 gtex_mr = uvmr(exp_name = "F2R Gtex str", out_name = kidney_outcomes)
 
+gtex_cross_mr = uvmr(exp_name = "F2R Gtex cross str",out_name = kidney_outcomes)
+
 # for weakly independent exposure ----------------------------------------------
+# multiple correlated SNPs from ukb-ppp, eqtlgen and gtex cross-tissue----------
 
 ukb_eqtl_mr_ld = rbind(uvmr_cor(exp_name = "F2R ukb wk", out_name = kidney_outcomes),
                        uvmr_cor(exp_name = "F2R eqtl wk", out_name = kidney_outcomes) )
 
-ukb_eqtl_mr = rbind(uvmr_cor(exp_name = "F2R ukb str", out_name = kidney_outcomes),
-                    uvmr_cor(exp_name = "F2R eqtl str", out_name = kidney_outcomes) )
+gtex_mr_ld = uvmr_cor(exp_name = "F2R Gtex cross wk",out_name = kidney_outcomes)
 
-kidney_mr = rbind(uvmr_cor(exp_name = "F2R kidney meta str",out_name = kidney_outcomes),
-                  uvmr_cor(exp_name = "F2R tubule meta str",out_name = kidney_outcomes) )
+# save results -----------------------------------------------------------------
 
-gtex_mr = uvmr_cor(exp_name = "F2R Gtex str",out_name = kidney_outcomes)
+par1_res =  rbind(ukb_eqtl_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+      kidney_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+      gtex_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+      gtex_cross_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+      ukb_eqtl_mr_ld,
+      gtex_mr_ld
+      ) %>% generate_odds_ratios()
 
 # write results ----------------------------------------------------------------
 
-data.table::fwrite(rbind(ukb_eqtl_mr%>%filter(method %in% c("Inverse variance weighted","Wald ratio"))%>%generate_odds_ratios,
-                         gtex_mr%>%filter(method %in% c("Inverse variance weighted","Wald ratio"))%>%generate_odds_ratios), 
-                   paste0(rdsf_personal,"./results/par1_kidney_abstract.csv"))
-
-
-uvmr(exp_name = "F2R ukb str", out_name = c("ieu-b-38", "ieu-b-39"))
-
-uvmr(exp_name = "F2R eqtl str", out_name = c("ieu-b-38","ieu-b-39"))
-
-uvmr(exp_name = "F2R kidney meta str", out_name = c("ieu-b-38","ieu-b-39"))
+data.table::fwrite(par1_res, paste0(rdsf_personal,"./results/par1_res.csv"))
