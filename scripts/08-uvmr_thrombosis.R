5d9ca987dd82265d5998c46a80ad69c42dfdab5c
Haotian <er20212@bristol.ac.uk>
2024-04-23 16:27:58 +0100

    write results for positive control

--- scripts/08-uvmr_thrombosis.R
+++ scripts/08-uvmr_thrombosis.R
@@ -17,19 +17,22 @@ gtex_pos_mr = uvmr(exp_name = "F2R Gtex str",out_name = thromb_outcomes)
 gtex_cross_pos_mr = uvmr(exp_name = "F2R Gtex cross str",out_name = thromb_outcomes)
 
 # for weakly independent exposure ----------------------------------------------
+# multiple correlated SNPs from ukb-ppp, eqtlgen and gtex cross-tissue----------
 
 ukb_eqtl_pos_mr_ld = rbind(uvmr_cor(exp_name = "F2R ukb wk", out_name = thromb_outcomes),
                        uvmr_cor(exp_name = "F2R eqtl wk", out_name = thromb_outcomes) )
 
-kidney_pos_mr_ld = rbind(uvmr_cor(exp_name = "F2R kidney meta str",out_name = thromb_outcomes),
-                      uvmr_cor(exp_name = "F2R tubule meta str",out_name = thromb_outcomes) )
-
-# gtex_pos_mr = uvmr_cor(exp_name = "F2R Gtex str",out_name = thromb_outcomes)
-
 gtex_cross_pos_mr_ld = uvmr_cor(exp_name = "F2R Gtex cross wk",out_name = thromb_outcomes)
+
+# save results -----------------------------------------------------------------
+
+par1_pos_res = rbind(ukb_eqtl_pos_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+                     kidney_pos_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+                     gtex_pos_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+                     gtex_cross_pos_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
+                     ukb_eqtl_pos_mr_ld,
+                     gtex_cross_pos_mr_ld) %>% generate_odds_ratios()
 
 # write results ----------------------------------------------------------------
 
-data.table::fwrite(rbind(ukb_eqtl_pos_mr%>%filter(method %in% c("Inverse variance weighted","Wald ratio"))%>%generate_odds_ratios,
-                         gtex_pos_mr%>%filter(method %in% c("Inverse variance weighted","Wald ratio"))%>%generate_odds_ratios), 
-                   paste0(rdsf_personal,"./results/par1_pos_abstract.csv"))
+data.table::fwrite(par1_pos_res, paste0(rdsf_personal,"./results/par1_pos_res.csv"))
