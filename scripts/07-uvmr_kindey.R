288c64809403589ae25b784583ccce93fbcbcaf3
Haotian <er20212@bristol.ac.uk>
2024-05-07 10:31:27 +0100

    delete gtex cross tissue part

--- scripts/07-uvmr_kindey.R
+++ scripts/07-uvmr_kindey.R
@@ -2,8 +2,7 @@
 
 f2r_all_instruments = fread(paste0(rdsf_personal,"data/par1/f2r_all_instruments.csv"))
 
-kidney_outcomes = c("ns_meta","ns","egfr_sd","ckd",
-                    "bun_sd","uacr","ma",
+kidney_outcomes = c("ns_meta","ns","egfr_sd","ckd","uacr","ma",
                     "ukb-d-30500_irnt","ukb-d-30600_irnt")
 
 # for strongly independent exposure --------------------------------------------
@@ -16,24 +15,18 @@ kidney_mr = rbind(uvmr(exp_name = "F2R kidney meta str", out_name = kidney_outco
 
 gtex_mr = uvmr(exp_name = "F2R Gtex str", out_name = kidney_outcomes)
 
-gtex_cross_mr = uvmr(exp_name = "F2R Gtex cross str",out_name = kidney_outcomes)
-
 # for weakly independent exposure ----------------------------------------------
 # multiple correlated SNPs from ukb-ppp, eqtlgen and gtex cross-tissue----------
 
 ukb_eqtl_mr_ld = rbind(uvmr_cor(exp_name = "F2R ukb wk", out_name = kidney_outcomes),
-                       uvmr_cor(exp_name = "F2R eqtl wk", out_name = kidney_outcomes) )
-
-gtex_mr_ld = uvmr_cor(exp_name = "F2R Gtex cross wk",out_name = kidney_outcomes)
+                       uvmr_cor(exp_name = "F2R eqtl wk", out_name = kidney_outcomes))
 
 # save results -----------------------------------------------------------------
 
 par1_res =  rbind(ukb_eqtl_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
       kidney_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
       gtex_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
-      gtex_cross_mr %>% filter(method %in% c("Inverse variance weighted","Wald ratio")),
-      ukb_eqtl_mr_ld,
-      gtex_mr_ld
+      ukb_eqtl_mr_ld
       ) %>% generate_odds_ratios()
 
 # write results ----------------------------------------------------------------
