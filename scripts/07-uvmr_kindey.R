4e862323b042d500d9635b467477f67b3555a10e
Haotian <er20212@bristol.ac.uk>
2024-05-07 14:50:18 +0100

    avoid sample overlap issue

--- scripts/07-uvmr_kindey.R
+++ scripts/07-uvmr_kindey.R
@@ -6,8 +6,9 @@ kidney_outcomes = c("ns_meta","ns","egfr_sd","ckd","uacr","ma",
                     "ukb-d-30500_irnt","ukb-d-30600_irnt")
 
 # for strongly independent exposure --------------------------------------------
+`%notin%` <- Negate(`%in%`)
 
-ukb_eqtl_mr = rbind(uvmr(exp_name = "F2R ukb str", out_name = kidney_outcomes),
+ukb_eqtl_mr = rbind(uvmr(exp_name = "F2R ukb str", out_name = kidney_outcomes[kidney_outcomes %notin% c("ns_meta","ukb-d-30500_irnt","ukb-d-30600_irnt")]),
                     uvmr(exp_name = "F2R eqtl str", out_name = kidney_outcomes) )
 
 kidney_mr = rbind(uvmr(exp_name = "F2R kidney meta str", out_name = kidney_outcomes),
@@ -18,7 +19,7 @@ gtex_mr = uvmr(exp_name = "F2R Gtex str", out_name = kidney_outcomes)
 # for weakly independent exposure ----------------------------------------------
 # multiple correlated SNPs from ukb-ppp, eqtlgen and gtex cross-tissue----------
 
-ukb_eqtl_mr_ld = rbind(uvmr_cor(exp_name = "F2R ukb wk", out_name = kidney_outcomes),
+ukb_eqtl_mr_ld = rbind(uvmr_cor(exp_name = "F2R ukb wk", out_name = kidney_outcomes[kidney_outcomes %notin% c("ns_meta","ukb-d-30500_irnt","ukb-d-30600_irnt")]),
                        uvmr_cor(exp_name = "F2R eqtl wk", out_name = kidney_outcomes))
 
 # save results -----------------------------------------------------------------
