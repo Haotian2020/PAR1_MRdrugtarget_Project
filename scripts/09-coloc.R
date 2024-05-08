920a699c8baf9eb8bb091f5331f8fb93d26bac19
Haotian <er20212@bristol.ac.uk>
2024-05-08 16:42:24 +0100

    Delete 09-coloc.R

--- scripts/09-coloc.R
+++
@@ -1,6 +0,0 @@
-# ukb is 38 build
-# neph2 is 37 build
-# need to liftover
-f2r_Glomneph2 = fread(paste0(rdsf_personal,"data/format_data/f2r_Glomneph2_GWAS_tidy_outcome.csv"))
-f2r_Tubeneph2 = fread(paste0(rdsf_personal,"data/format_data/f2r_Tubeneph2_GWAS_tidy_outcome.csv"))
-
